bddbb2b10f82ea2ac55ffa88fa8d16da72a21370
SeanMabli <smdrone1@gmail.com>
2021-11-04 18:57:23 -0400

    finshish

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ Display = pygame.display.set_mode([401, 401])
 Snake = np.zeros((1, 2))
 
 Score = 0
-Apple = np.zeros(2)
+Apple = np.ones(2)
 
 pygame.init()
 pygame.display.set_caption('Snake Game')
@@ -14,11 +14,23 @@ pygame.display.set_caption('Snake Game')
 done = False
 
 def ResetApplyPosition():
-  global Apple
+  global Apple, Snake, Score
+  if Snake[0, 0] == Apple[0] and Snake[0, 1] == Apple[1]:
+    Score += 1
+    Snake = np.vstack((Snake, Snake[-1]))
   for i in range(Snake.shape[0]):
-    if Apple.all(Snake[i, :]):
+    if Apple[0] == Snake[i, 0] and Apple[1] == Snake[i, 1]:
       Apple = np.random.randint(8, size=2)
       i = 0
+
+def GameOverCheck():
+  global done
+  if Snake[0, 0] < 0 or Snake[0, 0] > 7 or Snake[0, 1] < 0 or Snake[0, 1] > 7:
+    done = True
+  for i in range(Snake.shape[0] - 1):
+    for j in range(Snake.shape[0] - 1):
+      if i != j and Snake[i, 0] == Snake[j, 0] and Snake[i, 1] == Snake[j, 1]:
+          done = True
 
 while not done:
   for event in pygame.event.get():
@@ -47,6 +59,7 @@ while not done:
         Snake = np.vstack(([Snake[0, 0] - 1, Snake[0, 1]], Snake[:-1]))
 
     ResetApplyPosition()
+    GameOverCheck()
 
     pygame.display.flip()
 
