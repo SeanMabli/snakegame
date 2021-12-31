4aea610b72c3476cca52e802e6c734a85fdf6916
SeanMabli <smdrone1@gmail.com>
2021-12-31 07:03:00 -0500

    move ai back to aiinpy

--- python/ai.py
+++ python/ai.py
@@ -1,6 +1,9 @@
 import numpy as np
 import aiinpy as ai
 from alive_progress import alive_bar
+import wandb
+
+wandb.init(project="neuroevolution-snakegame")
 
 Board = 8
 popsize = 1000
@@ -16,65 +19,65 @@ for _ in range(popsize):
   Apple.append(x)
 
 model = ai.neuroevolution((8, 8), 4, popsize, [
-  ai.nn(outshape=16, activation=ai.relu(), learningrate=0.1),
-  ai.nn(outshape=16, activation=ai.relu(), learningrate=0.1),
-  ai.nn(outshape=16, activation=ai.relu(), learningrate=0.1),
-  ai.nn(outshape=4, activation=ai.stablesoftmax(), learningrate=0.1)
+  ai.rnn(outshape=4, Type='ManyToOne', outactivation=ai.stablesoftmax()),
 ])
 
 Turn = 0
 
-NumOfGen = 1000
+NumOfGen = 20000
 with alive_bar(NumOfGen) as bar:
   for gen in range(NumOfGen):
-    while np.sum(done) != popsize:
-      for player in range(popsize):
-        if done[player] == False:
-          input = np.zeros((Board, Board))
-          input[Apple[0][0], Apple[0][1]] = 0.5
-          for i in range(len(Snake[player])):
-            if not np.array_equal(Snake[player][i], [8, 8]):
-              input[int(Snake[player][i, 0]), int(Snake[player][i, 1])] = 1
+    for player in range(popsize):
+      input = np.zeros((5, Board, Board))
+      while done[player] == False:
+        input[:3] = input[1:]
+        input[4, Apple[0][0], Apple[0][1]] = 0.5
+        for i in range(len(Snake[player])):
+          if not np.array_equal(Snake[player][i], [8, 8]):
+            input[4, int(Snake[player][i, 0]), int(Snake[player][i, 1])] = 1
 
-          out = model.forwardsingle(input, player)
+        out = model.forwardsingle(input, player)
 
-          # Detect key commands
-          if np.max(out) == out[0]:
-            Snake[player] = np.vstack(([Snake[player][0, 0], Snake[player][0, 1] - 1], Snake[player][:-1]))
-          if np.max(out) == out[1]:
-            Snake[player] = np.vstack(([Snake[player][0, 0] + 1, Snake[player][0, 1]], Snake[player][:-1]))
-          if np.max(out) == out[2]:
-            Snake[player] = np.vstack(([Snake[player][0, 0], Snake[player][0, 1] + 1], Snake[player][:-1]))
-          if np.max(out) == out[3]:
-            Snake[player] = np.vstack(([Snake[player][0, 0] - 1, Snake[player][0, 1]], Snake[player][:-1]))
+        # Detect key commands
+        if np.max(out) == out[0]:
+          Snake[player] = np.vstack(([Snake[player][0, 0], Snake[player][0, 1] - 1], Snake[player][:-1]))
+        if np.max(out) == out[1]:
+          Snake[player] = np.vstack(([Snake[player][0, 0] + 1, Snake[player][0, 1]], Snake[player][:-1]))
+        if np.max(out) == out[2]:
+          Snake[player] = np.vstack(([Snake[player][0, 0], Snake[player][0, 1] + 1], Snake[player][:-1]))
+        if np.max(out) == out[3]:
+          Snake[player] = np.vstack(([Snake[player][0, 0] - 1, Snake[player][0, 1]], Snake[player][:-1]))
 
-          # Increase score
-          if np.array_equal(Snake[player][0, :], Apple[player]):
-            Score[player] += 1
-            Snake[player] = np.vstack((Snake[player], [Board, Board]))
+        # Increase score
+        if np.array_equal(Snake[player][0, :], Apple[player]):
+          Score[player] += 1
+          Snake[player] = np.vstack((Snake[player], [Board, Board]))
 
-          # Check that new apple location is not on the snake
-          i = 0
-          while i < Snake[player].shape[0]:
-            if Apple[player][0] == Snake[player][i, 0] and Apple[player][1] == Snake[player][i, 1]:
-              Apple[player] = np.random.randint(Board, size=2)
-              i = 0
-            i += 1
+        # Check that new apple location is not on the snake
+        i = 0
+        while i < Snake[player].shape[0]:
+          if Apple[player][0] == Snake[player][i, 0] and Apple[player][1] == Snake[player][i, 1]:
+            Apple[player] = np.random.randint(Board, size=2)
+            i = 0
+          i += 1
 
-          # Check out of bounds
-          if Snake[player][0, 0] < 0 or Snake[player][0, 0] > Board - 1 or Snake[player][0, 1] < 0 or Snake[player][0, 1] > Board - 1:
+        # Check out of bounds
+        if Snake[player][0, 0] < 0 or Snake[player][0, 0] > Board - 1 or Snake[player][0, 1] < 0 or Snake[player][0, 1] > Board - 1:
+          done[player] = True
+
+        # Check intersecting with itself
+        for i in range(1, Snake[player].shape[0]):
+          if np.array_equal(Snake[player][0, :], Snake[player][i, :]):
             done[player] = True
-
-          # Check intersecting with itself
-          for i in range(1, Snake[player].shape[0]):
-            if np.array_equal(Snake[player][0, :], Snake[player][i, :]):
-              done[player] = True
-      
-      Turn += 1
-      if Turn  == 100:
-        done = np.full(popsize, True)
     
+        Turn += 1
+        if Turn  == 100:
+          done[player] = True
+          Turn = 0
+
+    wandb.log({"sum score": np.sum(Score), "max score": np.max(Score)})
     print(np.sum(Score), np.max(Score))
+
     Score = np.zeros(popsize)
     done = np.full(popsize, False)
 
@@ -85,5 +88,4 @@ with alive_bar(NumOfGen) as bar:
       while np.sum(x) == 0:
         x = np.random.randint(Board, size=2)
       Apple.append(x)
-    Turn = 0
     bar()
